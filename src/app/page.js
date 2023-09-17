9863519a1e1cd246d937f3f9a0640e57d024491d
Utkarsh Pathak <98549357+utkarsh0428@users.noreply.github.com>
2023-09-17 15:45:22 +0530

    color and font changes

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,10 @@ import Panchang from './components/Panchang';
 export default  function Home() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center text-center p-[10px] mt-[220px] font-semibold text-sm font-Courgette text-white ">
+      <div className="flex flex-col justify-center items-center text-center p-[10px] mt-[220px] font-semibold text-sm font-Courgette bg-gradient-to-r from-green-300 to-red-300 bg-clip-text text-transparent">
         <h2>Welcome Seeker! Here you can feed your soul with the vibes of spiritual realm by watching interesting videos , reading blogs and knowing about today's panchang.</h2>
         <Panchang/>
       </div>
     </>
-
   )
 }
